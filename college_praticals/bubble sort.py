f7c56ed67d454bd2802db059a985f4bd95098414
adityaverma321 <59736486+adityaverma321@users.noreply.github.com>
2020-02-17 19:00:45 +0530

    Update bubble sort.py

--- college_praticals/bubble sort.py
+++ college_praticals/bubble sort.py
@@ -1,8 +1,8 @@
 def BubbleSort(A):
     for i in range(len(A)):
-        for k in range(len(A) - 1, i, -1):
-                if (A[k] < A[k - 1]):
-                    swap(A, k, k - 1)
+        for j in range(len(A) - 1, i, -1):
+                if (A[j] < A[j - 1]):
+                    swap(A, j, j - 1)
 
 def swap(A, x, y):
     tmp = A[x]
